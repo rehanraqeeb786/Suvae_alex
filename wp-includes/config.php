a2cd7386c01e65346349300f929995b1c1c23090
Rehan Raqeeb <raqeeb@dependibot.com>
2024-11-06 23:32:00 +0500

    laterst release

--- wp-includes/config.php
+++ wp-includes/config.php
@@ -1,5 +1,27 @@
 <?php
 // Define base URLs for different assets
-define('BASE_URL', 'http://localhost/suvae/highstreet.io/wp-includes/BlackLogo.svg');
-define('FOOTER_LOGO', 'http://localhost/suvae/highstreet.io/wp-includes/footer.svg');
+define('home_URL', 'http://52.66.82.140/highstreet.io/');
+define('BASE_URL', 'http://52.66.82.140/highstreet.io/wp-includes/BlackLogo.svg');
+define('FOOTER_LOGO', 'http://52.66.82.140/highstreet.io/wp-includes/footer.svg');
+define('PRODUCT_FEED_URL', 'http://52.66.82.140/highstreet.io/product-feed-management/');
+define('PRODUCT_LIST_URL', 'http://52.66.82.140/highstreet.io/product-feed-management/product-listings-ads/');
+define('PRODUCT_adretargeting_URL', 'http://52.66.82.140/highstreet.io/product-feed-management/ad-retargeting/');
+define('PRODUCT_affiliate_URL', 'http://52.66.82.140/highstreet.io/product-feed-management/affiliate-data-feed-management/');
+define('PRODUCT_social_URL', 'http://52.66.82.140/highstreet.io/product-feed-management/social-ecommerce/');
+define('PRODUCT_local_URL', 'http://52.66.82.140/highstreet.io/local-inventory-ads/');
+define('MARKETPLACE_URL', 'http://52.66.82.140/highstreet.io/marketplace-integration/');
+define('business_URL', 'http://52.66.82.140/highstreet.io/business-listing-management/');
+define('platform_URL', 'http://52.66.82.140/highstreet.io/platform/');
+define('marketing_URL', 'http://52.66.82.140/highstreet.io/marketing-channels/');
+define('marketplace_URL', 'http://52.66.82.140/highstreet.io/marketplace-channels/');
+define('ecommercemarketing_URL', 'http://52.66.82.140/highstreet.io/ecommerce-marketing-marketplace-blog/');
+define('customersuccess_URL', 'http://52.66.82.140/highstreet.io/customer-success-stories/');
+define('luxurybrands_URL', 'http://52.66.82.140/highstreet.io/the-luxury-brands-guide/');
+define('about_URL', 'http://52.66.82.140/highstreet.io/about-us/');
+define('JOB_URL', 'http://52.66.82.140/highstreet.io/about-us/jobs/');
+define('CONTACT_URL', 'http://52.66.82.140/highstreet.io/contact-us/');
+define('BOOK_URL', 'http://52.66.82.140/highstreet.io/book-a-demo/');
+define('privacy_URL', 'http://52.66.82.140/highstreet.io/privacy-policy/');
+define('firstimg_URL', 'http://52.66.82.140/highstreet.io/wp-includes/13.svg');
+define('secimg_URL', 'http://52.66.82.140/highstreet.io/wp-includes/12.svg');
 ?>
