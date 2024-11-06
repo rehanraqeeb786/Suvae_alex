2f8953abc243785efb0b7919f7d7cea1d80de94e
Rehan Raqeeb <raqeeb@dependibot.com>
2024-11-06 12:47:20 +0500

    init repo

---
+++ wp-includes/config.php
@@ -0,0 +1,5 @@
+<?php
+// Define base URLs for different assets
+define('BASE_URL', 'http://localhost/suvae/highstreet.io/wp-includes/BlackLogo.svg');
+define('FOOTER_LOGO', 'http://localhost/suvae/highstreet.io/wp-includes/footer.svg');
+?>
